f72abc6a45067669707b51203cd3aa520eff9ef9
dahemmer <david.hemmerijckx@gmail.com>
2016-06-06 19:38:49 +0200

    Add basic examples (binding, directives, events)

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ContactsComponent {
   message = 'Hello World!';
+  numbers = [1, 2, 3, 4, 5];
+
+  changeMessage(message: string) {
+    this.message = message;
+  }
 }
