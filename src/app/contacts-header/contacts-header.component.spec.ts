f9c8249a6d944401e447edf0a785bca13339df60
dahemmer <david.hemmerijckx@gmail.com>
2016-06-17 11:31:05 +0200

    Rewrite test

--- src/app/contacts-header/contacts-header.component.spec.ts
+++ src/app/contacts-header/contacts-header.component.spec.ts
@@ -1,46 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import {
-  beforeEach,
-  beforeEachProviders,
-  describe,
-  expect,
-  it,
-  inject,
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
 } from '@angular/core/testing';
-import { ComponentFixture, TestComponentBuilder } from '@angular/compiler/testing';
-import { Component } from '@angular/core';
-import { By } from '@angular/platform-browser';
+
 import { ContactsHeaderComponent } from './contacts-header.component';
 
-describe('Component: ContactsHeader', () => {
-  let builder: TestComponentBuilder;
-
-  beforeEachProviders(() => [ContactsHeaderComponent]);
-  beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
-    builder = tcb;
-  }));
-
-  it('should inject the component', inject([ContactsHeaderComponent],
-      (component: ContactsHeaderComponent) => {
+describe('Component: ContactsHeaderComponent', () => {
+  it('should create an instance', () => {
+    let component = new ContactsHeaderComponent();
     expect(component).toBeTruthy();
-  }));
-
-  it('should create the component', inject([], () => {
-    return builder.createAsync(ContactsHeaderComponentTestController)
-      .then((fixture: ComponentFixture<any>) => {
-        let query = fixture.debugElement.query(By.directive(ContactsHeaderComponent));
-        expect(query).toBeTruthy();
-        expect(query.componentInstance).toBeTruthy();
-      });
-  }));
+  });
 });
-
-@Component({
-  selector: 'dh-test',
-  template: `
-    <dh-contacts-header></dh-contacts-header>
-  `,
-  directives: [ContactsHeaderComponent]
-})
-class ContactsHeaderComponentTestController {
-}
-
