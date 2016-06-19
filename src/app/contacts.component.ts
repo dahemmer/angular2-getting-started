7338f5e5f740b69c705e201ea283e3f6d34da85a
dahemmer <david.hemmerijckx@gmail.com>
2016-06-19 16:49:22 +0200

    Use contact model in component

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ContactsHeaderComponent } from './contacts-header';
+import { Contact } from './model/contact';
 
 @Component({
   moduleId: module.id,
@@ -9,5 +10,23 @@ import { ContactsHeaderComponent } from './contacts-header';
   directives: [ContactsHeaderComponent]
 })
 export class ContactsComponent {
+  contact: Contact;
+  contacts: Array<Contact>;
 
+  constructor() {
+    this.contact = new Contact();
+    this.contact.name = 'John Doe';
+    this.contact.email = 'john.doe@mail.com';
+
+    this.contacts = new Array<Contact>();
+    this.contacts.push(this.contact);
+    this.contacts.push(<Contact>{
+      name: 'Jane Doe',
+      email: 'jane.doe@mail.com'
+    });
+    this.contacts.push(<Contact>{
+      name: 'Mike Doe',
+      email: 'mike.doe@mail.com'
+    });
+  }
 }
