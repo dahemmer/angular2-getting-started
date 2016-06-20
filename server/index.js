5f4388348cc5f062d1bfe9eedb3572bd005cb505
dahemmer <david.hemmerijckx@gmail.com>
2016-06-20 20:39:41 +0200

    Add bodyparser

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 var serveContacts = require('./mocks/contacts');
+var bodyParser = require('body-parser');
 
-module.exports = function(app) {
+module.exports = function (app) {
+  app.use(bodyParser.json());
   serveContacts(app);
 };
