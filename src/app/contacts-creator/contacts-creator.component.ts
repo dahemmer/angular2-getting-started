8f47c1c078b33ab52c99c9c5b3e57915e17d04fc
dahemmer <david.hemmerijckx@gmail.com>
2016-06-22 19:22:17 +0200

    Make form model driven

--- src/app/contacts-creator/contacts-creator.component.ts
+++ src/app/contacts-creator/contacts-creator.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router';
-import { Contact } from '../model';
 import { ContactsService } from '../service';
 
 @Component({
@@ -8,15 +8,38 @@ import { ContactsService } from '../service';
   selector: 'dh-contacts-creator',
   templateUrl: 'contacts-creator.component.html',
   styleUrls: ['contacts-creator.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
 })
-export class ContactsCreatorComponent {
+export class ContactsCreatorComponent implements OnInit {
+  form: FormGroup;
+  name: AbstractControl;
+
   constructor(
     private contactsService: ContactsService,
     private router: Router) {
   }
 
-  createContact(contact: Contact) {
-    this.contactsService.createContact(contact).subscribe(() => this.router.navigate(['']));
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      email: new FormControl(),
+      phone: new FormControl(),
+      address: new FormGroup({
+        street: new FormControl(),
+        number: new FormControl(),
+        zip: new FormControl(),
+        city: new FormControl(),
+        country: new FormControl(),
+      })
+    });
+
+    this.name = this.form.controls['name'];
+  }
+
+  createContact() {
+    this.contactsService.createContact(this.form.value).subscribe(() => this.router.navigate(['']));
   }
 }
